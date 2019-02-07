d58992dd2f31cce071cd2e8abf467bbc050d6a4a
Giovanni Gobbi <giovanni.gobbi3@studio.unibo.it>
2019-02-07 09:18:29 +0100

    Set pmd.xml file, findbugs local config and add main method

--- src/main/BarlugoFX.java
+++ src/main/BarlugoFX.java
@@ -1,9 +1,20 @@
 package main;
 
 /**
- * 
+ * The main class that contains the main method.
  *
  */
-public class BarlugoFX {
+final class BarlugoFX {
 
+    //to silence a checkstyle warning
+    private BarlugoFX() {
+        throw new AssertionError("This class isn't instantiable");
+    }
+
+    /**
+     * @param args: input parameters
+     */
+    public static void main(final String... args) {
+        System.out.println("Hello");
+    }
 }
