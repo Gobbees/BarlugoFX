d9680511cdad04dd49abf92ca4963b5e2169f39b
Matteo Fiordarancio <matteo.fiordarancio@studio.unibo.it>
2019-02-12 12:04:53 +0100

    added method to export image (bufferedImage dependent)

--- src/main/com/barlugofx/model/imageTools/ImageImpl.java
+++ src/main/com/barlugofx/model/imageTools/ImageImpl.java
@@ -2,6 +2,7 @@ package com.barlugofx.model.imageTools;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
 
 /**
  * A fast implementation of ImageImpl.
@@ -13,17 +14,31 @@ public final class ImageImpl implements Image {
     private static final int REDSHIFT = 16;
     private static final int GREENSHIFT = 8;
     private static final int ALPHASHIFT = 24;
-    private static final int ALPHAVALUE = -16777216; //If the alpha channel is not present, this means that the alpha is 255, in a 32 bit
-    // integer, this means that the first 8 are at 1, which is exactly the number above
+    private static final int ALPHAVALUE = -16777216; // If the alpha channel is not present, this means that the alpha
+    // is 255, in a 32 bit
+    // integer, this means that the first 8 are at 1, which is exactly the number
+    // above
 
     private final BufferedImage image;
     private final int width;
     private final int height;
+    private final boolean hasAlphaChannel;
+    private final int[][] pixels;
 
     private ImageImpl(final BufferedImage image) {
         this.image = image;
         width = image.getWidth();
         height = image.getHeight();
+        hasAlphaChannel = image.getAlphaRaster() != null;
+        pixels = constructPixels();
+    }
+
+    private ImageImpl(final BufferedImage image, final int[][] pixels) {
+        this.image = image;
+        width = image.getWidth();
+        height = image.getHeight();
+        hasAlphaChannel = image.getAlphaRaster() != null;
+        this.pixels = pixels;
     }
 
     /**
@@ -32,14 +47,46 @@ public final class ImageImpl implements Image {
      * @param image the bufferedImage to startWith.
      * @return a new builded image.
      */
-    public static Image imageBuilder(final BufferedImage image) {
+    public static Image buildFromBufferedImage(final BufferedImage image) {
         return new ImageImpl(image);
+    }
+
+    /**
+     * Builds an image starting from its matrix of pixels.
+     *
+     * @param pixels the matrix from which we build on
+     * @return a new Image
+     */
+    public static Image buildFromPixels(final int[][] pixels) {
+        final int width = pixels[0].length;
+        final int height = pixels.length;
+        final BufferedImage target = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        final int[] targetPixel = ((DataBufferInt) target.getRaster().getDataBuffer()).getData();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                targetPixel[i * width + j] = pixels[i][j];
+            }
+        }
+        return new ImageImpl(target, pixels);
     }
 
     @Override
     public int[][] getImageRGBvalues() {
+        return pixels;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    private int[][] constructPixels() {
         final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-        final boolean hasAlphaChannel = image.getAlphaRaster() != null;
 
         final int[][] result = new int[height][width];
         if (hasAlphaChannel) {
@@ -75,15 +122,5 @@ public final class ImageImpl implements Image {
         }
 
         return result;
-    }
-
-    @Override
-    public int getWidth() {
-        return width;
-    }
-
-    @Override
-    public int getHeight() {
-        return height;
     }
 }
