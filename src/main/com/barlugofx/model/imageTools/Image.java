fa3b09f642ec3a4e0e52bfc8ebe71d5470199d10
Matteo Fiordarancio <matteo.fiordarancio@studio.unibo.it>
2019-02-12 14:45:38 +0100

    Added some documentation on ImageImpl
    if the order of the pixel is not the one expected than we have an unexpected behaviour

--- src/main/com/barlugofx/model/imageTools/Image.java
+++ src/main/com/barlugofx/model/imageTools/Image.java
@@ -8,6 +8,9 @@ package com.barlugofx.model.imageTools;
 public interface Image {
     /**
      * A method returning the RGB value for each pixel of an image.
+     * Note that the image is assumed (like it is commonly do) as a matrix containing a byte alpha (if present), a byte for blue,
+     * a byte for green and a byte for red.
+     * If the order is modified than any operation done on the matrix could retrieve unexpected result.
      * @return a matrix width*height containing all the pixels.
      */
     int[][] getImageRGBvalues();
