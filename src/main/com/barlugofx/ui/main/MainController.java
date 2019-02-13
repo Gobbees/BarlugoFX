ea27282d7dba5a1772cbbbaf477f3d4b2c0bddf1
Giovanni Gobbi <giovanni.gobbi3@studio.unibo.it>
2019-02-13 20:13:56 +0100

    Add a super temp view to test tools. Don't look the code please.

---
+++ src/main/com/barlugofx/ui/main/MainController.java
@@ -0,0 +1,102 @@
+package com.barlugofx.ui.main;
+
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
+import com.barlugofx.model.imageTools.Image;
+import com.barlugofx.model.imageTools.ImageUtilities;
+import com.barlugofx.model.tools.Brightness;
+import com.barlugofx.model.tools.common.ImageFilter;
+import com.barlugofx.model.tools.common.ParameterImpl;
+import com.barlugofx.model.tools.common.ParametersName;
+import com.barlugofx.ui.ViewController;
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.beans.binding.Bindings;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ *
+ */
+public class MainController implements ViewController {
+    @FXML
+    private ScrollPane spTools;
+    
+    @FXML
+    private SplitPane spRightColumn;
+    
+    @FXML
+    private AnchorPane apane;
+    
+    @FXML
+    private JFXSlider slider;
+    
+    @FXML
+    private JFXTextField tf;
+    
+    @FXML
+    private ImageView imageView;
+    //temp
+    private Image image;
+
+
+    /* (non-Javadoc)
+     * @see com.barlugofx.ui.ViewController#setStage(javafx.stage.Stage)
+     */
+    @Override
+    public void setStage(final Stage stage) {
+        slider.setValue(0);
+        slider.setMin(-100);
+        slider.setMax(100);
+        spTools.setFitToWidth(true);
+        apane.setPrefHeight(100);
+        spRightColumn.widthProperty().addListener((a, b, c) -> System.out.println(c));
+        spTools.setMinWidth(200);
+        tf.setText(slider.valueProperty().intValue() + "");
+        slider.valueProperty().addListener((ev, ov, nv) -> {
+            tf.setText(nv.intValue() + "");
+            //temp
+            apply(nv.intValue());
+            //
+        });
+        tf.textProperty().addListener((ev, ov, nv) -> {
+            try {
+                slider.setValue(Integer.parseInt(nv));
+                tf.setStyle("-fx-text-fill:#333130");
+                //temp
+                apply(Integer.parseInt(nv));
+                //
+            } catch (NumberFormatException e) {
+                tf.setStyle("-fx-text-fill:red");
+            }
+        });
+ 
+        
+    }
+    //temp
+    private void apply(int value) {
+        final ImageFilter contrast = Brightness.createBrightnees();
+        contrast.addParameter(ParametersName.BRIGHTNESS, new ParameterImpl<>(122));
+        imageView.setImage(SwingFXUtils.toFXImage(ImageUtilities.convertImageToBufferedImageWithAlpha(contrast.applyFilter(image)), null));
+        
+    }
+    //temp
+    public void setImage(Image im) {
+        image = im;
+    }
+    //
+    
+  
+    
+    
+
+}
--- src/main/com/barlugofx/ui/main/MainView.java
+++ src/main/com/barlugofx/ui/main/MainView.java
@@ -2,17 +2,29 @@ package com.barlugofx.ui.main;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
+
+import com.barlugofx.model.imageTools.Image;
+import com.barlugofx.model.imageTools.ImageImpl;
+import com.barlugofx.model.imageTools.ImageUtilities;
+import com.barlugofx.model.tools.Brightness;
+import com.barlugofx.model.tools.common.ImageFilter;
+import com.barlugofx.model.tools.common.ParameterImpl;
+import com.barlugofx.model.tools.common.ParametersName;
 import com.barlugofx.ui.Animations;
 import com.barlugofx.ui.loading.LoadingView;
 
+import apple.laf.JRSUIUtils.ScrollBar;
 import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -36,20 +48,34 @@ public class MainView {
      */
     public MainView(final Stage stage, final File file) throws IOException {
         //init stage
+        LoadingView t = new LoadingView(stage);
         this.stage = stage;
         stage.setTitle(STAGE_TITLE);
+        //temp
+        final BufferedImage image = ImageIO.read(file);
+        final Image toWorkWith = ImageImpl.buildFromBufferedImage(image);
         //
         screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-        LoadingView t = new LoadingView(stage);
+        final FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(new URL("file:res/fxml/FXMLMain.fxml"));
+        root = loader.load();
+        scene = new Scene(root, screenDimension.getWidth(), screenDimension.getHeight());
+        MainController mc = loader.getController();
+        mc.setImage(toWorkWith);
+        mc.setStage(stage);
+        
+        
         //load file, do initial operations (history init, ecc) by controller (to do)
         t.stop();
-        root = FXMLLoader.load(new URL("file:res/fxml/FXMLMain.fxml"));
-        scene = new Scene(root, screenDimension.getWidth(), screenDimension.getHeight());
+        
         Timeline tl = Animations.resizeToFullScreen(Duration.millis(ANIM_MILLIS), stage, ANIM_STEP, screenDimension);
         tl.setOnFinished(e -> {
             stage.setScene(scene);
         });
         tl.play();
+        
         stage.show();
+        
+        
     }
 }
