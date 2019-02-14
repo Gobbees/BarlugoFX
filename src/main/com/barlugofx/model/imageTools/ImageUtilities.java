453b8058030c806e960f0928ba26e029e4f1bd7f
Matteo Fiordarancio <matteo.fiordarancio@studio.unibo.it>
2019-02-14 11:23:06 +0100

    Created basic converter from rgb to hsv

--- src/main/com/barlugofx/model/imageTools/ImageUtilities.java
+++ src/main/com/barlugofx/model/imageTools/ImageUtilities.java
@@ -1,5 +1,6 @@
 package com.barlugofx.model.imageTools;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 
@@ -62,5 +63,40 @@ public final class ImageUtilities {
             }
         }
         return target;
+    }
+
+    /**
+     * This method convert a matrix of pixels written as RGB into an equivalent matrix of float value ins HSB, with H value being the
+     * [i][j][0],  S value [i][j][1] and B value [i][j][2].This functions uses the static method RGBtoHSB of java.awt.Color .
+     * @param pixels the matrix to convert
+     * @return the converted float matrix.
+     */
+    public static float[][][] rgbToHsb(final int[][] pixels) {
+        final float[][][] result = new float[pixels.length][pixels[0].length][3];
+        final ColorManipulator getter = ColorManipulatorImpl.createColorExtractor();
+        for (int i = 0; i < pixels.length; i++) {
+            for (int j = 0; j < pixels[0].length; j++) {
+                Color.RGBtoHSB(getter.getRed(pixels[i][j]), getter.getGreen(pixels[i][j]), getter.getBlue(pixels[i][j]),
+                        result[i][j]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * This method converts hsb values into a matrix of rgb pixels. The hsv values has to be passed as a 3-D float array, in which the
+     * [i][j][0] value corresponds to Hue, the [i][j][1] to Saturation and the [i][j][2]  to Brightness. Alpha value (if present) is not
+     * handled.
+     * @param pixelsHSB the float 3-D array that we need to convert.
+     * @return the converted pixels matrix
+     */
+    public static int[][] hsbToRgb(final float[][][] pixelsHSB){
+        final int[][] pixels = new int[pixelsHSB.length][pixelsHSB[0].length];
+        for (int i = 0; i < pixels.length; i++) {
+            for (int j = 0; j < pixels[0].length; j++) {
+                pixels[i][j] = Color.HSBtoRGB(pixelsHSB[i][j][0], pixelsHSB[i][j][1], pixelsHSB[i][j][2]);
+            }
+        }
+        return pixels;
     }
 }
