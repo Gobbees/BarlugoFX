62ee1d11580ad01dfeda63485f94d0a7d888498f
Ugo Baroncini <ugobaroncini@gmail.com>
2019-03-10 21:07:49 +0100

    History and SequenceNode implementation template.
    Added History and SequenceNode implementation template, sketched out private fields.

---
+++ src/main/barlugofx/model/history/HistoryImpl.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package barlugofx.model.history;
+
+import barlugofx.model.tools.common.Parameter;
+import java.util.List;
+
+/**
+ *
+ *
+ */
+public class HistoryImpl implements History {
+    private List<SequenceNode> tools;
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.History#addFilter(barlugofx.model.history.SequenceNode)
+     */
+    @Override
+    public void addFilter(SequenceNode node) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.History#deleteTool(int)
+     */
+    @Override
+    public void deleteTool(int index) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.History#hideTool(int)
+     */
+    @Override
+    public void hideTool(int index) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.History#showTool(int)
+     */
+    @Override
+    public void showTool(int index) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.History#findByName(java.lang.String)
+     */
+    @Override
+    public int findByName(String toolName) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.History#editTool(int, barlugofx.model.history.SequenceNode)
+     */
+    @Override
+    public void editTool(int index, SequenceNode node) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.History#getValue(java.lang.String)
+     */
+    @Override
+    public Parameter<? extends Number> getValue(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/main/barlugofx/model/history/SequenceNodeImpl.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package barlugofx.model.history;
+
+import barlugofx.model.imagetools.Image;
+import barlugofx.model.tools.common.ImageTool;
+
+/**
+ *
+ *
+ */
+public class SequenceNodeImpl implements SequenceNode {
+    private Image startImage;
+    private Boolean isActive;
+    private String filterName;
+    private ImageTool tool;
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.SequenceNode#getStartImage()
+     */
+    @Override
+    public Image getStartImage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.SequenceNode#setStartImage()
+     */
+    @Override
+    public void setStartImage() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.SequenceNode#isActive()
+     */
+    @Override
+    public Boolean isActive() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.SequenceNode#getNodeName()
+     */
+    @Override
+    public String getNodeName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.SequenceNode#setNodeName(java.lang.String)
+     */
+    @Override
+    public void setNodeName(String name) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see barlugofx.model.history.SequenceNode#getTool()
+     */
+    @Override
+    public ImageTool getTool() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
