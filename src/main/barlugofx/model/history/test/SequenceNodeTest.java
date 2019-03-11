dfbd28525db5f7251e0ed9ba3acd18dfc31f4229
Matteo Fiordarancio - matteo.fiordarancio@studio.unibo.it <matteo.fiordarancio@studio.unibo.it>
2019-03-11 13:47:27 +0100

    created test for sequence node

--- src/main/barlugofx/model/history/test/SequenceNodeTest.java
+++ src/main/barlugofx/model/history/test/SequenceNodeTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import barlugofx.model.history.SequenceNode;
 import barlugofx.model.history.SequenceNodeImpl;
 import barlugofx.model.imagetools.Image;
+import barlugofx.model.imagetools.ImageImpl;
+import barlugofx.model.tools.BlackAndWhite;
 import barlugofx.model.tools.common.ImageTool;
 
 
@@ -15,14 +17,17 @@ import barlugofx.model.tools.common.ImageTool;
  */
 public final class SequenceNodeTest {
     private static final String DEFAULT_NAME = "TEST";
-    private static final Image DEFAULT_IMAGE = null;
-    private static final ImageTool DEFAULT_TOOL = null;
+    private static final Image DEFAULT_IMAGE = ImageImpl.buildFromPixels(new int[2][2]);
+    private static final ImageTool DEFAULT_TOOL = BlackAndWhite.createBlackAndWhite();
 
     /**
      * Testing inizialization.
      */
     @Test
     public void checkInizialization() {
+        /*
+         *TODO : If this test passed than all the equals are implemented correctly
+         */
         final SequenceNode node = new SequenceNodeImpl(DEFAULT_NAME, DEFAULT_TOOL, DEFAULT_IMAGE);
         Assert.assertTrue(node.isActive());
         Assert.assertSame(DEFAULT_NAME, node.getNodeName());
@@ -30,6 +35,54 @@ public final class SequenceNodeTest {
         Assert.assertSame(DEFAULT_TOOL, node.getTool());
     }
 
+    /**
+     * Testing set and get.
+     */
+    @Test
+    public void testSetAndGet() {
+        final SequenceNode node = new SequenceNodeImpl(DEFAULT_NAME, DEFAULT_TOOL, DEFAULT_IMAGE);
+        node.setNodeName("CIAO");
+        Assert.assertTrue(node.getNodeName().equals("CIAO"));
+        final Image newS = ImageImpl.buildFromPixels(new int[3][3]);
+        node.setStartImage(newS);
+        Assert.assertSame(node.getStartImage(), newS);
+        try {
+            node.setStartImage(null);
+            Assert.fail("Node should not accept null values");
+        } catch (final IllegalArgumentException e) {
+            Assert.assertTrue(true);
+        } catch (final Exception e) {
+            Assert.fail();
+        }
+        node.disable();
+        Assert.assertFalse(node.isActive());
+        node.enable();
+        Assert.assertTrue(node.isActive());
+    }
 
+    /**
+     * Testing null values.
+     */
+    @Test
+    public void testNullValues() {
+        try {
+            final SequenceNode node = new SequenceNodeImpl(null, DEFAULT_TOOL, DEFAULT_IMAGE);
+            Assert.fail();
+        } catch (final IllegalArgumentException e) {
+            Assert.assertTrue(true);
+        }
+        try {
+            final SequenceNode node = new SequenceNodeImpl(DEFAULT_NAME, DEFAULT_TOOL, null);
+            Assert.fail();
+        } catch (final IllegalArgumentException e) {
+            Assert.assertTrue(true);
+        }
+        try {
+            final SequenceNode node = new SequenceNodeImpl(DEFAULT_NAME, null, DEFAULT_IMAGE);
+            Assert.fail();
+        } catch (final IllegalArgumentException e) {
+            Assert.assertTrue(true);
+        }
+    }
 
 }
