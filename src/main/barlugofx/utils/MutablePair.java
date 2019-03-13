e1ecfeff6ff43b5feb5e53c58b6735d5bc89f7e8
Matteo Fiordarancio <matteo.fiordarancio@studio.unibo.it>
2019-03-13 14:05:41 +0100

    Update mutable pair

--- src/main/barlugofx/utils/MutablePair.java
+++ src/main/barlugofx/utils/MutablePair.java
@@ -1,12 +1,19 @@
 package barlugofx.utils;
 
 /**
- * This class implements a mutable pair, so it is possible to change both its values.
+ * This class implements a mutable pair, so it is possible to change both its
+ * values. Be careful though that this Pair is not safe for any set.
+ *
+ * Note: Great care must be exercised if mutable objects are used as set
+ * elements. The behavior of a set is not specified if the value of an object is
+ * changed in a manner that affects equals comparisons while the object is an
+ * element in the set. A special case of this prohibition is that it is not
+ * permissible for a set to contain itself as an element.
  *
  * @param <F> the first value type
  * @param <S> the second value type
  */
-public class MutablePair<F, S> {
+public final class MutablePair<F, S> {
     private F first;
     private S second;
     /**
@@ -43,5 +50,43 @@ public class MutablePair<F, S> {
      */
     public void setSecond(final S second) {
         this.second = second;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (first == null ? 0 : first.hashCode());
+        result = prime * result + (second == null ? 0 : second.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof MutablePair)) {
+            return false;
+        }
+        final MutablePair other = (MutablePair) obj;
+        if (first == null) {
+            if (other.first != null) {
+                return false;
+            }
+        } else if (!first.equals(other.first)) {
+            return false;
+        }
+        if (second == null) {
+            if (other.second != null) {
+                return false;
+            }
+        } else if (!second.equals(other.second)) {
+            return false;
+        }
+        return true;
     }
 }
