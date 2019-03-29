fbad43a14ab6670e1b32b660bc9fb043bb38cb69
Giovanni Gobbi <giovanni.gobbi3@studio.unibo.it>
2019-03-29 19:35:16 +0100

    Big view improvement
    Now the loading view shows properly while the controller loads the image in RAM

--- src/main/barlugofx/view/main/MainView.java
+++ src/main/barlugofx/view/main/MainView.java
@@ -21,7 +21,7 @@ import javafx.util.Duration;
  */
 public class MainView extends AbstractView<MainController> {
     //private constant fields
-    private static final double ANIM_MILLIS = 600.0;
+    private static final double ANIM_MILLIS = 400.0;
     private static final double ANIM_STEP = 50.0;
 
     /**
@@ -32,15 +32,6 @@ public class MainView extends AbstractView<MainController> {
     public MainView(final Stage stage, final File file) {
         super("BarlugoFX", "file:res/img/logo.png", stage, new Dimension((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(), 
                             (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight() - (stage.getHeight() - stage.getScene().getHeight()))));
-        AppManager manager;
-        try {
-            manager = new AppManagerImpl(file);
-        } catch (IOException e) {
-            //TODO log
-            //TODO error view
-            e.printStackTrace();
-            return;
-        }
         final LoadingView t = new LoadingView(stage);
         try {
             this.loadFXML("file:res/fxml/FXMLMain.fxml");
@@ -48,25 +39,40 @@ public class MainView extends AbstractView<MainController> {
             //log!!!!!
             e.printStackTrace();
         }
-        final FadeTransition loadingOut = t.getFadeOutTransition();
-        loadingOut.setOnFinished(fadeOutEvent -> {
-            this.getStage().setScene(null);
-            this.getStage().setResizable(true);
-            //this is performed after the animation finish because if not the view is closed too strongly.
-            final Timeline stageTimeline = AnimationUtils.resizeToFullScreen(Duration.millis(ANIM_MILLIS), stage, ANIM_STEP, Toolkit.getDefaultToolkit().getScreenSize());
-            stageTimeline.setOnFinished(timelineEvent -> {
-                final FadeTransition mainIn = AnimationUtils.fadeInTransition(Duration.millis(ANIM_MILLIS), this.getScene().getRoot());
-                mainIn.play();
-                this.getStage().setScene(this.getScene());
-                //calls the controller setStage function after the scene set because I need the components sizes on the screen, and they are initialized only with the new scene set
+        new Thread(() ->  {
+            AppManager manager;
+            try {
+                manager = new AppManagerImpl(file);
+            } catch (IOException e) {
+                //TODO log
+                //TODO error view
+                e.printStackTrace();
+                return;
+            }
+            final FadeTransition loadingOut = t.getFadeOutTransition();
+            loadingOut.setOnFinished(fadeOutEvent -> {
                 Platform.runLater(() -> {
+                    this.getStage().setScene(null);
+                    this.getStage().setResizable(true);
+                });
+                //this is performed after the animation finish because if not the view is closed too strongly.
+                final Timeline stageTimeline = AnimationUtils.resizeToFullScreen(Duration.millis(ANIM_MILLIS), stage, ANIM_STEP, Toolkit.getDefaultToolkit().getScreenSize());
+                stageTimeline.setOnFinished(timelineEvent -> {
+                    final FadeTransition mainIn = AnimationUtils.fadeInTransition(Duration.millis(ANIM_MILLIS), this.getScene().getRoot());
+                    this.getStage().setScene(this.getScene());
+                    Platform.runLater(() -> {
+                    mainIn.play();
+                    //calls the controller setStage function after the scene set because I need the components sizes on the screen, and they are initialized only with the new scene set
                     this.getController().setStage(this.getStage());
                     this.getController().setManager(manager);
+                    });
+                });
+                Platform.runLater(() -> { 
+                    stageTimeline.play();
+                    this.getStage().show();
                 });
             });
-            stageTimeline.play();
-            this.getStage().show();
-        });
-        loadingOut.play();
+            Platform.runLater(() -> loadingOut.play());
+        }).start();
     }
 }
