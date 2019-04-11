0afd487a4c120b80a3a0080fc2e2fda015d10399
Giovanni Gobbi <giovanni.gobbi3@studio.unibo.it>
2019-04-11 20:42:02 +0200

    Add rotate indicator (probably temp)

--- src/main/barlugofx/view/main/components/tools/RotateLine.java
+++ src/main/barlugofx/view/main/components/tools/RotateLine.java
@@ -1,7 +1,9 @@
 package barlugofx.view.main.components.tools;
 
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
@@ -13,11 +15,16 @@ public final class RotateLine implements ComplexNode {
     //constant fields
     private static final int LINE_WIDTH = 2;
     private static final int CIRCLE_WIDTH = 4;
-    //
+    private static final int INDICATOR_BG_SIZE = 20;
+    private static final int LABEL_SHIFTER = 30;
+    private static final Paint LIGHT_BLUE_COLOR = Color.web("rgb(90,255,208)");
     private final Line line;
     private final Circle start;
     private final Circle end;
-    /**
+    private final Circle circle;
+    private final Label degrees;
+    private double angle;
+	/**
      * The class constructor.
      * @param startX the start X coordinate
      * @param startY the start Y coordinate
@@ -28,6 +35,11 @@ public final class RotateLine implements ComplexNode {
         line = createLine(startX, startY, endX, endY);
         start = createPoint(startX, startY);
         end = createPoint(endX, endY);
+        circle = new Circle(INDICATOR_BG_SIZE);
+        circle.setStroke(LIGHT_BLUE_COLOR);
+        circle.setFill(Color.BLACK);
+        degrees = new Label();
+        degrees.setTextFill(LIGHT_BLUE_COLOR);
     }
     /**
      * Returns the line.
@@ -50,17 +62,37 @@ public final class RotateLine implements ComplexNode {
     public Circle getEndPoint() {
         return end;
     }
+    
+    public double getAngle() {
+		return angle;
+	}
+    public void drag(final double x, final double y) {
+    	line.setEndX(x);
+    	line.setEndY(y);
+    	end.setCenterX(x);
+    	end.setCenterY(y);
+    	degrees.setLayoutX(x - LABEL_SHIFTER);
+        degrees.setLayoutY(y - LABEL_SHIFTER);
+        circle.setCenterX(degrees.getLayoutX() + degrees.getWidth() / 2);
+        circle.setCenterY(degrees.getLayoutY() + degrees.getHeight() / 2);
+    	angle = computeAngle(start.getCenterX(), start.getCenterY(), x, y);
+        degrees.setText(String.format("%1$,.2f°",angle));
+    }
     @Override
     public void addToPane(final Pane pane) {
         pane.getChildren().add(line);
         pane.getChildren().add(start);
         pane.getChildren().add(end);
+        pane.getChildren().add(circle);
+        pane.getChildren().add(degrees);
     }
     @Override
     public void removeFromPane(final Pane pane) {
         pane.getChildren().remove(line);
         pane.getChildren().remove(start);
         pane.getChildren().remove(end);
+        pane.getChildren().remove(circle);
+        pane.getChildren().remove(degrees);
     }
     private static Circle createPoint(final double x, final double y) {
         final Circle c = new Circle(x, y, CIRCLE_WIDTH);
@@ -73,5 +105,16 @@ public final class RotateLine implements ComplexNode {
         l.setStroke(Color.WHITE);
         l.setStrokeWidth(LINE_WIDTH);
         return l;
+    }
+    private double computeAngle(final double x1, final double y1, final double x2, final double y2) {
+    	double m;
+        if (y2 < start.getCenterY()) {
+            m = Math.abs(y2 - y1) / (x2 - x1);
+        } else if (x2 < x1) {
+            m = (y2 - y1) / Math.abs(x2 - x1);
+        } else {
+            m = -(y2 - y1) / (x2 - x1);
+        }
+        return Math.atan(m) * (180 / Math.PI);
     }
 }
