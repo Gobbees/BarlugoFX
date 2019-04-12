6e8b51d9fff434f9062db30028659422f34c3af5
Ugo Baroncini <ugobaroncini@gmail.com>
2019-04-12 18:51:38 +0200

    Created model-side tool enum
    The view-side enum has a field for each color channel in both BLACKANDWHITE and SELECTIVECOLOR.

---
+++ src/main/barlugofx/model/tools/Tools.java
@@ -0,0 +1,16 @@
+package barlugofx.model.tools;
+
+/**
+ * This enumeration contains all the supported tool names (model side).
+ */
+enum Tool {
+    EXPOSURE,
+    CONTRAST,
+    BRIGHTNESS,
+    WHITEBALANCE,
+    SATURATION,
+    HUE,
+    VIBRANCE,
+    SELECTIVECOLOR,
+    BLACKANDWHITE
+}
