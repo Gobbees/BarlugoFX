b0d89a980026ce822e8e972ee499ed944885a932
Ugo Baroncini <ugobaroncini@gmail.com>
2019-04-12 19:10:51 +0200

    Fixed enum name, public, Javadoc
    Missing s in enum name, wasn't matching file name.
    Enum made public.
    Added Javadoc comment to each enum field.

--- src/main/barlugofx/model/tools/Tools.java
+++ src/main/barlugofx/model/tools/Tools.java
@@ -3,14 +3,49 @@ package barlugofx.model.tools;
 /**
  * This enumeration contains all the supported tool names (model side).
  */
-enum Tool {
+public enum Tools {
+    /**
+     * Enum for the Exposure tool.
+     */
     EXPOSURE,
+
+    /**
+     * Enum for the Contrast tool.
+     */
     CONTRAST,
+
+    /**
+     * Enum for the Brightness tool.
+     */
     BRIGHTNESS,
+
+    /**
+     * Enum for the White Balance tool.
+     */
     WHITEBALANCE,
+
+    /**
+     * Enum for the Saturation tool.
+     */
     SATURATION,
+
+    /**
+     * Enum for the Hue tool.
+     */
     HUE,
+
+    /**
+     * Enum for the Vibrance tool.
+     */
     VIBRANCE,
+
+    /**
+     * Enum for the SelectiveColor tool.
+     */
     SELECTIVECOLOR,
+
+    /**
+     * Enum for the Black and White tool.
+     */
     BLACKANDWHITE
 }
