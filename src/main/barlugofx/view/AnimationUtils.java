27ffa6781c2e98504d3e296c054196bad38fc038
Giovanni Gobbi <giovanni.gobbi3@studio.unibo.it>
2019-04-22 18:53:31 +0200

    View animation change
    -setmaximized does it right

--- src/main/barlugofx/view/AnimationUtils.java
+++ src/main/barlugofx/view/AnimationUtils.java
@@ -1,14 +1,7 @@
 package barlugofx.view;
 
-import java.awt.Dimension;
-
 import javafx.animation.FadeTransition;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.beans.value.WritableValue;
 import javafx.scene.Node;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
 /**
@@ -45,49 +38,5 @@ public final class AnimationUtils {
         ft.setFromValue(1);
         ft.setToValue(0);
         return ft;
-    }
-
-    /**
-     * Returns a Timeline that resizes the stage to a certain dimension.
-     * 
-     * @param duration       the duration of the timeline
-     * @param stage          the stage that will be resized
-     * @param step           the step of each increment
-     * @param finalDimension the final dimension of the stage after the resize
-     * @return the animation Timeline
-     */
-    public static Timeline resizeToFullScreen(final Duration duration, final Stage stage, final double step,
-            final Dimension finalDimension) {
-        final WritableValue<Double> writableWidth = new WritableValue<Double>() {
-            @Override
-            public Double getValue() {
-                return stage.getWidth();
-            }
-
-            @Override
-            public void setValue(final Double value) {
-                stage.setWidth(value);
-                stage.centerOnScreen();
-            }
-        };
-        final WritableValue<Double> writableHeight = new WritableValue<Double>() {
-            @Override
-            public Double getValue() {
-                return stage.getHeight();
-            }
-
-            @Override
-            public void setValue(final Double value) {
-                stage.setHeight(value);
-                stage.centerOnScreen();
-            }
-        };
-        final Timeline tl = new Timeline();
-        tl.getKeyFrames()
-                .addAll(new KeyFrame(Duration.ZERO, new KeyValue(writableWidth, writableWidth.getValue() + step),
-                        new KeyValue(writableHeight, writableHeight.getValue() + step)),
-                        new KeyFrame(duration, new KeyValue(writableWidth, finalDimension.getWidth()),
-                                new KeyValue(writableHeight, finalDimension.getHeight())));
-        return tl;
     }
 }
