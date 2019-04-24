0f0d1a24a75ab5c6743c1c47e463a4ee593429cf
Giovanni Gobbi <giovanni.gobbi3@studio.unibo.it>
2019-04-24 22:25:57 +0200

    Controller update

--- src/main/barlugofx/controller/AppManagerImpl.java
+++ src/main/barlugofx/controller/AppManagerImpl.java
@@ -7,12 +7,17 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 import barlugofx.model.imagetools.Image;
 import barlugofx.model.imagetools.ImageUtils;
 import barlugofx.model.parallelhandler.ParallelFilterExecutor;
+import barlugofx.model.procedure.AdjustmentAlreadyPresentException;
+import barlugofx.model.procedure.AdjustmentImpl;
+import barlugofx.model.procedure.Procedure;
+import barlugofx.model.procedure.ProcedureImpl;
 import barlugofx.model.tools.BlackAndWhite;
 import barlugofx.model.tools.Brightness;
 import barlugofx.model.tools.Contrast;
@@ -22,10 +27,12 @@ import barlugofx.model.tools.Hue;
 import barlugofx.model.tools.Rotator;
 import barlugofx.model.tools.Saturation;
 import barlugofx.model.tools.SelectiveRGBChanger;
+import barlugofx.model.tools.Tools;
 import barlugofx.model.tools.Vibrance;
 import barlugofx.model.tools.WhiteBalance;
 import barlugofx.model.tools.common.ImageTool;
 import barlugofx.model.tools.common.ParallelizableImageTool;
+import barlugofx.model.tools.common.Parameter;
 import barlugofx.model.tools.common.ParameterImpl;
 import barlugofx.model.tools.common.ParameterName;
 import barlugofx.utils.Format;
@@ -35,63 +42,46 @@ import barlugofx.view.View;
  * The main controller class.
  */
 public final class AppManagerImpl implements AppManager {
-    //multipliers used to adapt the input from the view to the model
+    // multipliers used to adapt the input from the view to the model
     private static final float HSB_MULTIPLIER = 0.01f;
+    private static final float EXPOSURE_MULTIPLIER = 0.005f;
     private static final float WB_MULTIPLIER = 0.015f;
     private static final float VIBRANCE_MULTIPLIER = 0.01f;
     private static final float BW_MULTIPLIER = 0.004f;
     private static final float BW_SHIFTER = 0.8f;
 
     private Image image;
-    //Tools
-    private final ParallelizableImageTool exposure;
-    private final ParallelizableImageTool contrast;
-    private final ParallelizableImageTool brightness;
-    private final ImageTool wb;
-    private final ParallelizableImageTool saturation;
-    private final ParallelizableImageTool hue;
-    private final ParallelizableImageTool srgb;
-    private final ParallelizableImageTool bw;
-    private final ImageTool cropper;
-    private final ImageTool rotator;
-    private final ParallelizableImageTool vibrance;
     private final IOManager fileManager;
-    private final ParallelFilterExecutor executor;
-    private final boolean parallel;
+    private Procedure procedure;
+
     /**
-     * The constructor of the class. It takes the input file chosen by the user and initiates all the elements
+     * The constructor of the class. It takes the input file chosen by the user and
+     * initiates all the elements
+     * 
      * @param file the input file
      * @throws IOException if the file opening fails
      */
     public AppManagerImpl(final File file) throws IOException {
         fileManager = new IOManagerImpl();
         setImage(file);
-        exposure = Exposure.createExposure();
-        contrast = Contrast.createContrast();
-        brightness = Brightness.createBrightness();
-        wb = WhiteBalance.createWhiteBalance();
-        srgb = SelectiveRGBChanger.createSelective();
-        bw = BlackAndWhite.createBlackAndWhite();
-        saturation = Saturation.createSaturation();
-        hue = Hue.createHue();
-        cropper = Cropper.createCropper();
-        rotator = Rotator.createRotator();
-        vibrance = Vibrance.createVibrance();
-        executor = ParallelFilterExecutor.executor();
-        parallel = ParallelFilterExecutor.shouldYouParallelize(image);
     }
+
     @Override
     public Image getImage() {
         return image;
     }
+
     @Override
     public BufferedImage getBufferedImage() {
         return ImageUtils.convertImageToBufferedImageWithAlpha(image);
     }
+
     @Override
-    public void setImage(final File file) throws IOException {  //TODO reset history and all components
+    public void setImage(final File file) throws IOException {
         image = fileManager.loadImageFromFile(file);
+        procedure = new ProcedureImpl(image, ParallelFilterExecutor.shouldYouParallelize(image));
     }
+
     @Override
     public String getInputFileName() {
         return fileManager.getFileName();
@@ -102,13 +92,9 @@ public final class AppManagerImpl implements AppManager {
         if (value == Exposure.getDefaultValue()) {
             return;
         }
-       exposure.addParameter(ParameterName.EXPOSURE, new ParameterImpl<Float>(value * HSB_MULTIPLIER));
-       if (parallel) {
-           image = executor.applyTool(exposure, image);
-       } else {
-           image = exposure.applyTool(image);
-       }
-       exposure.removeParameter(ParameterName.EXPOSURE);
+        final Exposure exposure = Exposure.createExposure();
+        exposure.addParameter(ParameterName.EXPOSURE, new ParameterImpl<Float>(value * HSB_MULTIPLIER));
+        image = uploadProcedure(Tools.EXPOSURE, exposure);
     }
 
     @Override
@@ -116,13 +102,9 @@ public final class AppManagerImpl implements AppManager {
         if (value == Contrast.getDefaultValue()) {
             return;
         }
+        final Contrast contrast = Contrast.createContrast();
         contrast.addParameter(ParameterName.CONTRAST, new ParameterImpl<Integer>(value));
-        if (parallel) {
-            image = executor.applyTool(contrast, image);
-        } else {
-            image = contrast.applyTool(image);
-        }
-        contrast.removeParameter(ParameterName.CONTRAST);
+        image = uploadProcedure(Tools.CONTRAST, contrast);
     }
 
     @Override
@@ -130,13 +112,9 @@ public final class AppManagerImpl implements AppManager {
         if (value == Brightness.getDefaultValue()) {
             return;
         }
+        final Brightness brightness = Brightness.createBrightness();
         brightness.addParameter(ParameterName.BRIGHTNESS, new ParameterImpl<Integer>(value));
-        if (parallel) {
-            image = executor.applyTool(brightness, image);
-        } else {
-            image = brightness.applyTool(image);
-        }
-        brightness.removeParameter(ParameterName.BRIGHTNESS);
+        image = uploadProcedure(Tools.BRIGHTNESS, brightness);
     }
 
     @Override
@@ -144,9 +122,9 @@ public final class AppManagerImpl implements AppManager {
         if (value == WhiteBalance.getDefaultValue()) {
             return;
         }
+        final WhiteBalance wb = WhiteBalance.createWhiteBalance();
         wb.addParameter(ParameterName.WHITEBALANCE, new ParameterImpl<Float>(value * WB_MULTIPLIER));
-        image = wb.applyTool(image);
-        wb.removeParameter(ParameterName.WHITEBALANCE);
+        image = uploadProcedure(Tools.WHITEBALANCE, wb);
     }
 
     @Override
@@ -154,13 +132,9 @@ public final class AppManagerImpl implements AppManager {
         if (value == Saturation.getDefaultValue()) {
             return;
         }
-        saturation.addParameter(ParameterName.SATURATION, new ParameterImpl<Float>(value * HSB_MULTIPLIER));
-        if (parallel) {
-            image = executor.applyTool(saturation, image);
-        } else {
-            image = saturation.applyTool(image);
-        }
-        saturation.removeParameter(ParameterName.SATURATION);
+        final Saturation saturation = Saturation.createSaturation();
+        saturation.addParameter(ParameterName.SATURATION, new ParameterImpl<Float>(value * EXPOSURE_MULTIPLIER));
+        image = uploadProcedure(Tools.SATURATION, saturation);
     }
 
     @Override
@@ -168,13 +142,9 @@ public final class AppManagerImpl implements AppManager {
         if (value == Hue.getDefaultValue()) {
             return;
         }
+        final Hue hue = Hue.createHue();
         hue.addParameter(ParameterName.HUE, new ParameterImpl<Float>(value * HSB_MULTIPLIER));
-        if (parallel) {
-            image = executor.applyTool(hue, image);
-        } else {
-            image = hue.applyTool(image);
-        }
-        hue.removeParameter(ParameterName.HUE);
+        image = uploadProcedure(Tools.HUE, hue);
     }
 
     @Override
@@ -182,83 +152,99 @@ public final class AppManagerImpl implements AppManager {
         if (value == Vibrance.getDefaultValue()) {
             return;
         }
+        final Vibrance vibrance = Vibrance.createVibrance();
         vibrance.addParameter(ParameterName.VIBRANCE_INCREMENT, new ParameterImpl<Float>(value * VIBRANCE_MULTIPLIER));
-        if (parallel) {
-            image = executor.applyTool(vibrance, image);
-        } else {
-            image = vibrance.applyTool(image);
-        }
-        vibrance.removeParameter(ParameterName.VIBRANCE_INCREMENT);
+        image = uploadProcedure(Tools.VIBRANCE, vibrance);
     }
 
     @Override
     public void setSelectiveColors(final int r, final int g, final int b) {
-        //TODO make it possible to change only one of these.
-        if (r == SelectiveRGBChanger.getDefaultValue() || g == SelectiveRGBChanger.getDefaultValue() || b == SelectiveRGBChanger.getDefaultValue()) {
+        if (r == SelectiveRGBChanger.getDefaultValue() && g == SelectiveRGBChanger.getDefaultValue()
+                && b == SelectiveRGBChanger.getDefaultValue()) {
             return;
         }
+        final SelectiveRGBChanger srgb = SelectiveRGBChanger.createSelective();
         srgb.addParameter(ParameterName.RED, new ParameterImpl<Integer>(r));
         srgb.addParameter(ParameterName.GREEN, new ParameterImpl<Integer>(g));
         srgb.addParameter(ParameterName.BLUE, new ParameterImpl<Integer>(b));
-        if (parallel) {
-            image = executor.applyTool(srgb, image);
-        } else {
-            image = srgb.applyTool(image);
-        }
-        srgb.removeParameter(ParameterName.RED);
-        srgb.removeParameter(ParameterName.GREEN);
-        srgb.removeParameter(ParameterName.BLUE);
+        image = uploadProcedure(Tools.SELECTIVECOLOR, srgb);
     }
 
     @Override
     public void setBlackAndWhite(final double r, final double g, final double b) {
-        if (r == BlackAndWhite.getDefaultValue() || g == BlackAndWhite.getDefaultValue() || b == BlackAndWhite.getDefaultValue()) {
+        if (r == BlackAndWhite.getDefaultValue() || g == BlackAndWhite.getDefaultValue()
+                || b == BlackAndWhite.getDefaultValue()) {
             return;
         }
+        final BlackAndWhite bw = BlackAndWhite.createBlackAndWhite();
         bw.addParameter(ParameterName.WRED, new ParameterImpl<Double>(r * BW_MULTIPLIER + BW_SHIFTER));
         bw.addParameter(ParameterName.WGREEN, new ParameterImpl<Double>(g * BW_MULTIPLIER + BW_SHIFTER));
         bw.addParameter(ParameterName.WBLUE, new ParameterImpl<Double>(b * BW_MULTIPLIER + BW_SHIFTER));
-        if (parallel) {
-            image = executor.applyTool(bw, image);
-        } else {
-            image = bw.applyTool(image);
-        }
-        bw.removeParameter(ParameterName.WRED);
-        bw.removeParameter(ParameterName.WGREEN);
-        bw.removeParameter(ParameterName.WBLUE);
+        image = uploadProcedure(Tools.BLACKANDWHITE, bw);
     }
 
     @Override
-    public void exportImage(final File file, final Format format) throws IOException, InterruptedException, ExecutionException {
+    public void exportImage(final File file, final Format format)
+            throws IOException, InterruptedException, ExecutionException {
         fileManager.exportImage(image, file, format);
     }
 
     @Override
-    public void exportImage(final File file, final float quality) throws IOException, InterruptedException, ExecutionException {
+    public void exportImage(final File file, final float quality)
+            throws IOException, InterruptedException, ExecutionException {
         fileManager.exportJPEGWithQuality(image, file, quality);
     }
+
     @Override
     public void rotate(final double angle) {
-       rotator.addParameter(ParameterName.ANGLE, new ParameterImpl<Double>(angle));
-       image = rotator.applyTool(image);
-       rotator.removeParameter(ParameterName.ANGLE);
+        final Rotator rotator = Rotator.createRotator();
+        if (!procedure.canAdd(Tools.ROTATOR)) {
+            final Optional<Parameter<? extends Number>> oldAngle = procedure.getValue(Tools.ROTATOR, ParameterName.ANGLE);
+            if (oldAngle.isPresent()) {
+                rotator.addParameter(ParameterName.ANGLE, new ParameterImpl<Double>(angle + oldAngle.get().getValue().doubleValue()));
+            } else {
+                rotator.addParameter(ParameterName.ANGLE, new ParameterImpl<Double>(angle));
+            }
+        } else {
+            rotator.addParameter(ParameterName.ANGLE, new ParameterImpl<Double>(angle));
+        }
+        image = uploadProcedure(Tools.ROTATOR, rotator);
     }
+
     @Override
     public void crop(final int x1, final int y1, final int x2, final int y2) {
-        cropper.addParameter(ParameterName.X1, new ParameterImpl<Integer>(x1));
-        cropper.addParameter(ParameterName.X2, new ParameterImpl<Integer>(x2));
-        cropper.addParameter(ParameterName.Y1, new ParameterImpl<Integer>(y1));
-        cropper.addParameter(ParameterName.Y2, new ParameterImpl<Integer>(y2));
-        image = cropper.applyTool(image);
-        cropper.removeParameter(ParameterName.X1);
-        cropper.removeParameter(ParameterName.X2);
-        cropper.removeParameter(ParameterName.Y1);
-        cropper.removeParameter(ParameterName.Y2);
+        final Cropper cropper = Cropper.createCropper();
+        if (!procedure.canAdd(Tools.CROPPER)) {
+            final Optional<Parameter<? extends Number>> oldX1 = procedure.getValue(Tools.CROPPER, ParameterName.X1);
+            final Optional<Parameter<? extends Number>> oldY1 = procedure.getValue(Tools.CROPPER, ParameterName.Y1);
+            final Optional<Parameter<? extends Number>> oldX2 = procedure.getValue(Tools.CROPPER, ParameterName.X2);
+            final Optional<Parameter<? extends Number>> oldY2 = procedure.getValue(Tools.CROPPER, ParameterName.Y1);
+            if (oldX1.isPresent() && oldY1.isPresent() && oldX2.isPresent() && oldY2.isPresent()) {
+                cropper.addParameter(ParameterName.X1, new ParameterImpl<Integer>(oldX1.get().getValue().intValue() + x1));
+                cropper.addParameter(ParameterName.X2, new ParameterImpl<Integer>(oldX2.get().getValue().intValue() - x2));
+                cropper.addParameter(ParameterName.Y1, new ParameterImpl<Integer>(oldY1.get().getValue().intValue() + y1));
+                cropper.addParameter(ParameterName.Y2, new ParameterImpl<Integer>(oldY2.get().getValue().intValue() - y2));
+            } else {
+                cropper.addParameter(ParameterName.X1, new ParameterImpl<Integer>(x1));
+                cropper.addParameter(ParameterName.X2, new ParameterImpl<Integer>(x2));
+                cropper.addParameter(ParameterName.Y1, new ParameterImpl<Integer>(y1));
+                cropper.addParameter(ParameterName.Y2, new ParameterImpl<Integer>(y2));
+            }
+        } else {
+            cropper.addParameter(ParameterName.X1, new ParameterImpl<Integer>(x1));
+            cropper.addParameter(ParameterName.X2, new ParameterImpl<Integer>(x2));
+            cropper.addParameter(ParameterName.Y1, new ParameterImpl<Integer>(y1));
+            cropper.addParameter(ParameterName.Y2, new ParameterImpl<Integer>(y2));
+        }
+        image = uploadProcedure(Tools.CROPPER, cropper);
     }
+
     @Override
-    public void savePreset(final Properties filters, final File file) throws IOException, InterruptedException, ExecutionException {
+    public void savePreset(final Properties filters, final File file)
+            throws IOException, InterruptedException, ExecutionException {
         fileManager.writePreset(filters, file);
     }
+
     @Override
     public void applyPreset(final File file) throws IOException {
         final Properties properties = fileManager.loadPreset(file);
@@ -273,8 +259,8 @@ public final class AppManagerImpl implements AppManager {
             final Enumeration<?> e = properties.propertyNames();
             while (e.hasMoreElements()) {
                 filterName = (String) e.nextElement();
-                ///TODO Remove print
-                //System.out.println(filterName + properties.getProperty(filterName));
+                /// TODO Remove print
+                // System.out.println(filterName + properties.getProperty(filterName));
                 if (filterName.equals("SelectiveColors")) {
                     intValues = Arrays.asList(properties.getProperty(filterName).split(",")).stream()
                             .mapToInt(Integer::parseInt).toArray();
@@ -298,5 +284,27 @@ public final class AppManagerImpl implements AppManager {
             e.printStackTrace();
         }
     }
-    //TODO history
+    //Model choices. I cannot delete this copy paste.
+    private Image uploadProcedure(final Tools toolType, final ParallelizableImageTool tool) {
+        if (procedure.canAdd(toolType)) {
+            try {
+                return procedure.add(new AdjustmentImpl(tool));
+            } catch (AdjustmentAlreadyPresentException e) {
+                throw new IllegalStateException();
+            }
+        } else {
+            return procedure.edit(toolType, new AdjustmentImpl(tool));
+        }
+    }
+    private Image uploadProcedure(final Tools toolType, final ImageTool tool) {
+        if (procedure.canAdd(toolType)) {
+            try {
+                return procedure.add(new AdjustmentImpl(tool));
+            } catch (AdjustmentAlreadyPresentException e) {
+                throw new IllegalStateException();
+            }
+        } else {
+            return procedure.edit(toolType, new AdjustmentImpl(tool));
+        }
+    }
 }
