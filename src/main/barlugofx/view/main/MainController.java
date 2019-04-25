86ea41a19f9a17e6c228a16b5a6ff7c1a994484b
Giovanni Gobbi <giovanni.gobbi3@studio.unibo.it>
2019-04-25 15:06:17 +0200

    Set spane width to scene

--- src/main/barlugofx/view/main/MainController.java
+++ src/main/barlugofx/view/main/MainController.java
@@ -699,6 +699,7 @@ public final class MainController extends AbstractViewControllerWithManager {
         spaneRightColumn.setMaxWidth(this.getScene().getWidth() * RIGHT_COLUMN_MAX_MULTIPLIER);
         spaneMain.setMaxWidth(this.getScene().getWidth());
         lvHistory.setPrefHeight(spaneRightColumn.getHeight() - (spaneRightColumn.getHeight() * spaneRightColumn.getDividers().get(0).getPosition()) - lblHistory.getHeight() - btnUndo.getHeight());
+        spaneMain.setPrefWidth(this.getScene().getWidth());
     }
 
     private void initToolStatus() {
